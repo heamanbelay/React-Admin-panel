89c23b15e6b80d6a519d2e1acebc10100011b7eb
heamanbelay <heamanbelay@gmail.com>
2022-10-11 13:38:57 +0300

    message

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+        <Route  path='/user/:userId' element={<User/>}  />       
+          <Route  path='/' element={<Home/>}  />       
+          <Route path='/users' element={<UserList/>}  />  
+          <Route path='/products' element={<ProductList/>}  />  
+          <Route path='/product/:productId' element={<Product/>}  />
+          <Route path='/newproduct' element={<NewProduct/>}  />
+          
+        </Routes>
+
+        </div>
+    </Router>
+    );
+  }
+  
+  export default App;
+  
+    {/* <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+          <Routes>  
+          <Route  path='/' element={<Home/>}  /> ,        
+          <Route path='/users' element={<UserList/>}  />  ,      
+         </Routes> */}
+
+  // return (
+  //   <div className="App">
+  //   <Topbar/>
+
+  //     <div className="container">
+  //      <Sidebar/>
+  //    </div>
+  //   </div>
+  
